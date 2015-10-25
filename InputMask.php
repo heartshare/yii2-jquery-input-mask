ad2d4e3747b53c4ec876703c8ef8a36eb9b194d4
Ivan Chistyakov <ivan.chkv@gmail.com>
2015-10-25 07:18:48 +0300

    fix InputMask.php

--- InputMask.php
+++ InputMask.php
@@ -4,12 +4,22 @@ namespace yii\jquery\inputmask;
 
 use yii\helpers\Html;
 use yii\widgets\InputWidget;
+use yii\helpers\Json;
+use Yii;
 
 class InputMask extends InputWidget
 {
 
+    /**
+     * @var string|null
+     * @see https://github.com/RobinHerbots/jquery.inputmask/blob/3.x/README.md
+     */
     public $alias = null;
 
+    /**
+     * @var string|\yii\web\JsExpression|null
+     * @see https://github.com/RobinHerbots/jquery.inputmask/blob/3.x/README.md
+     */
     public $mask = null;
 
     /**
@@ -24,5 +34,43 @@ class InputMask extends InputWidget
     {
         Html::addCssClass($this->options, 'form-control');
         parent::init();
+    }
+
+    /**
+     * @inheritdoc
+     */
+    public function run()
+    {
+        $inputId = $this->options['id'];
+        $hasModel = $this->hasModel();
+        if (array_key_exists('value', $this->options)) {
+            $value = $this->options['value'];
+        } elseif ($hasModel) {
+            $value = Html::getAttributeValue($this->model, $this->attribute);
+        } else {
+            $value = $this->value;
+        }
+        $options = array_merge($this->options, ['value' => $value]);
+        if ($hasModel) {
+            $output = Html::activeTextInput($this->model, $this->attribute, $options);
+        } else {
+            $output = Html::textInput($this->name, $this->value, $options);
+        }
+        $clientOptions = array_merge($this->clientOptions, [
+            'alias' => $this->alias,
+            'mask' => $this->mask
+        ]);
+        if (!array_key_exists('placeholder', $clientOptions) && array_key_exists('placeholder', $options)) {
+            $clientOptions['placeholder'] = $options['placeholder'];
+        }
+        $js = 'jQuery(\'#' . $inputId . '\').inputmask(' . Json::htmlEncode($clientOptions) . ');';
+        if (Yii::$app->getRequest()->getIsAjax()) {
+            $output .= Html::script($js);
+        } else {
+            $view = $this->getView();
+            InputMaskAsset::register($view);
+            $view->registerJs($js);
+        }
+        return $output;
     }
 }
